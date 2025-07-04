a24a2624bb1027475f6b3d21671c7ba684068194
cypheribbi <bsef22m547@pucit.edu.pk>
2025-07-04 15:58:18 +0500

    Added Header and Footer in layout.tsx and added my Vercel Token

--- Internship/assignment1/src/app/layout.tsx
+++ Internship/assignment1/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Separator } from "@/components/ui/separator"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,11 +24,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <header className="w-full shadow p-4 bg-white/80 backdrop-blur">
+        <div className="max-w-4xl mx-auto text-xl font-bold text-gray-800 text-center">
+          Welcome to Muhammad Ibrahim&apos;s Assignment 1
+        </div>
+      </header>
+       <Separator />
+
         {children}
+            <footer className="w-full bg-white/80 backdrop-blur border-t mt-8 py-4">
+        <div className="max-w-4xl mx-auto text-center text-sm text-gray-500">
+          © {new Date().getFullYear()} Muhammad Ibrahim. All rights reserved.
+        </div>
+      </footer>
       </body>
+   
     </html>
   );
 }
