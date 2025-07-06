828896dedc3f1b36382748320d07f66fdf23e1c0
cypheribbi <bsef22m547@pucit.edu.pk>
2025-07-06 20:45:13 +0500

    Changing Heading Colour

--- Internship/assignment1/src/app/layout.tsx
+++ Internship/assignment1/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
          <header className="w-full shadow p-4 bg-orange-200 backdrop-blur">
-        <div className="max-w-4xl mx-auto text-xl font-bold text-gray-800 text-center">
+        <div className="max-w-4xl mx-auto text-xl font-bold text-blue-950 text-center">
           Welcome to Muhammad Ibrahim&apos;s Assignment 1
         </div>
       </header>
